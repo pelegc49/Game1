290b895b0f86c2c2aa9c6f9f0fea6d84cfd0f11c
pelegc49 <pelegc49@gmail.com>
2022-02-05 01:30:53 +0200

    Made car rotate

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -11,8 +11,13 @@ public class Driver : MonoBehaviour
     }
 
     // Update is called once per frame
+    float x = 0f;
     void Update()
     {
         
+        transform.Rotate(0, 0, 0.1f);
+        transform.Translate(0, 0.05f, 0);
+        //transform.Translate(0.1f*Mathf.Sin(x),0.2f*Mathf.Cos(x), 0);
+        x += 0.01f;
     }
 }
