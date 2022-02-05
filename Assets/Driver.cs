256d1c25a131f87370d4ae78c01b68166f9232b3
pelegc49 <pelegc49@gmail.com>
2022-02-05 02:53:23 +0200

    Added accelaration

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    const float MAX_SPEED = 0.15f;
+
+    float turnSpeed = 0.1f;
+    float moveSpeed = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    float x = 0f;
+
     void Update()
     {
-        
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.05f, 0);
-        //transform.Translate(0.1f*Mathf.Sin(x),0.2f*Mathf.Cos(x), 0);
-        x += 0.01f;
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0);
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W)) && moveSpeed * 1.1f < MAX_SPEED)
+        {
+            moveSpeed *= 1.001f;
+            Debug.Log("increasing speed");
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S))
+        {
+            moveSpeed *= 0.999f;
+            Debug.Log("Decreasing speed");
+        }
+        Debug.Log(moveSpeed);
     }
 }
+
