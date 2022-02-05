f32b66f86ffac50bd905dc144517acbe8a5ee2a6
pelegc49 <pelegc49@gmail.com>
2022-02-05 16:41:21 +0200

    Added camera follow and a background image

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -7,6 +7,7 @@ public class Driver : MonoBehaviour
     const float MAX_SPEED = 0.115f;
     const float MIN_TURN = -0.15f;
     const float MAX_TURN = -1 * MIN_TURN;
+    float fuel = 50;
 
     float turnSpeed = 0.1f;
     float moveSpeed = 0.05f;
@@ -20,16 +21,20 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
+
+
         transform.Rotate(0, 0, turnSpeed);
         transform.Translate(0, moveSpeed, 0);
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W)) && moveSpeed * 1.001f < MAX_SPEED)
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKey(KeyCode.W)) && moveSpeed * 1.001f < MAX_SPEED && fuel > 0)
         {
             if (moveSpeed == 0)
             {
                 moveSpeed = 0.007f;
             }
             moveSpeed *= 1.001f;
-            Debug.Log("increasing speed");
+            fuel -= 0.01f;
+
+            //Debug.Log("increasing speed");
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKey(KeyCode.S))
         {
@@ -41,7 +46,7 @@ public class Driver : MonoBehaviour
             {
                 moveSpeed *= 0.998f;
             }
-            Debug.Log("Decreasing speed");
+            //Debug.Log("Decreasing speed");
         }
         else { 
             if (moveSpeed<0.008){
@@ -51,20 +56,25 @@ public class Driver : MonoBehaviour
             }
         if (moveSpeed < 0.0005f) moveSpeed = 0;
 
-        Debug.Log(moveSpeed);
-        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A)) && turnSpeed <= MAX_TURN)
+        Debug.Log(fuel);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            turnSpeed += 0.01f;
+            turnSpeed = 0;
         }
         else if ((Input.GetKeyDown(KeyCode.D) || Input.GetKey(KeyCode.D)) && turnSpeed >= MIN_TURN)
         {
             turnSpeed -= 0.01f;
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+        else  if ((Input.GetKeyDown(KeyCode.A) || Input.GetKey(KeyCode.A)) && turnSpeed <= MAX_TURN)
         {
-            turnSpeed = 0;
+            turnSpeed += 0.01f;
         }
         else if (moveSpeed == 0) turnSpeed = 0;
+
+
+        if (fuel < 0){
+            fuel=0;
+        }
     }
 }
 
---
+++ Assets/FollowCam.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+[SerializeField] GameObject toFollow;
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position = toFollow.transform.position + new Vector3(0,0,-1);
+    }
+}
